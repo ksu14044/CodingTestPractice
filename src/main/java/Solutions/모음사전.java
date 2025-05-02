35041961cc83daa567f6cfeb8280d0a0f0ecc7dd
ksu14044 <mhm1404@naver.com>
2025-05-02 20:35:25 +0900

    모음 사전

---
+++ src/main/java/Solutions/모음사전.java
@@ -0,0 +1,39 @@
+package Solutions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 모음사전 {
+    public static void main(String[] args) {
+        String[] ahdma = {"A", "E", "I", "O", "U"};
+        List<String> dict = new ArrayList<>();
+        for(int i = 0 ; i < ahdma.length; i++){
+            String a = "";
+            a += ahdma[i];
+            dict.add(a);
+            for(int j = 0 ; j < ahdma.length ; j++){
+                String b = a;
+                b += ahdma[j];
+                dict.add(b);
+                for(int k = 0 ; k < ahdma.length ; k++){
+                    String c = b;
+                    c += ahdma[k];
+                    dict.add(c);
+                    for(int p = 0 ; p < ahdma.length ; p++){
+                        String d = c;
+                        d += ahdma[p];
+                        dict.add(d);
+                        for(int q = 0 ; q < ahdma.length ;q++){
+                            String e = d;
+                            e += ahdma[q];
+                            dict.add(e);
+                        }
+                    }
+                }
+
+            }
+
+        }
+        System.out.println(dict.indexOf("I"));
+    }
+}
