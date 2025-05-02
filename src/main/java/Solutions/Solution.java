9f929ab5be6827def43a0db2adeb7e7d98ddfb01
ksu14044 <mhm1404@naver.com>
2025-05-02 10:25:30 +0900

    2016년

---
+++ src/main/java/Solutions/Solution.java
@@ -0,0 +1,19 @@
+package Solutions;
+
+import java.util.Date;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] month = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int a = 5;
+        int b = 24;
+        int days = 0;
+        for(int i = 1; i < a ; i++){
+            days += month[i];
+        }
+        days += b - 1;
+        String[] day = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        int index = days % 7;
+        System.out.println(day[index]);
+    }
+}
