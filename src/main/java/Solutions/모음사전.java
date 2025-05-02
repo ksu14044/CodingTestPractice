d21c52c36ed637b2f5bad902ddf106917563653e
ksu14044 <mhm1404@naver.com>
2025-05-02 20:44:27 +0900

    모ã음 사전 ã완전탐색

--- src/main/java/Solutions/모음사전.java
+++ src/main/java/Solutions/모음사전.java
@@ -4,36 +4,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class 모음사전 {
+    static List<String> dictionary = new ArrayList<>();
+    static char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+
     public static void main(String[] args) {
-        String[] ahdma = {"A", "E", "I", "O", "U"};
-        List<String> dict = new ArrayList<>();
-        for(int i = 0 ; i < ahdma.length; i++){
-            String a = "";
-            a += ahdma[i];
-            dict.add(a);
-            for(int j = 0 ; j < ahdma.length ; j++){
-                String b = a;
-                b += ahdma[j];
-                dict.add(b);
-                for(int k = 0 ; k < ahdma.length ; k++){
-                    String c = b;
-                    c += ahdma[k];
-                    dict.add(c);
-                    for(int p = 0 ; p < ahdma.length ; p++){
-                        String d = c;
-                        d += ahdma[p];
-                        dict.add(d);
-                        for(int q = 0 ; q < ahdma.length ;q++){
-                            String e = d;
-                            e += ahdma[q];
-                            dict.add(e);
-                        }
-                    }
-                }
-
-            }
-
-        }
-        System.out.println(dict.indexOf("I"));
+        dfs("", 0);
+        System.out.println(dictionary.indexOf("aaaae"));
     }
+
+    public static void dfs(String cur, int length){
+        if(length > 5) return;
+        dictionary.add(cur);
+        for(char c : vowels){
+            dfs(cur + c, length +1);
+        }
+    }
+
 }
+    //    public static void main(String[] args) {
+//        String[] ahdma = {"A", "E", "I", "O", "U"};
+//        List<String> dict = new ArrayList<>();
+//        for(int i = 0 ; i < ahdma.length; i++){
+//            String a = "";
+//            a += ahdma[i];
+//            dict.add(a);
+//            for(int j = 0 ; j < ahdma.length ; j++){
+//                String b = a;
+//                b += ahdma[j];
+//                dict.add(b);
+//                for(int k = 0 ; k < ahdma.length ; k++){
+//                    String c = b;
+//                    c += ahdma[k];
+//                    dict.add(c);
+//                    for(int p = 0 ; p < ahdma.length ; p++){
+//                        String d = c;
+//                        d += ahdma[p];
+//                        dict.add(d);
+//                        for(int q = 0 ; q < ahdma.length ;q++){
+//                            String e = d;
+//                            e += ahdma[q];
+//                            dict.add(e);
+//                        }
+//                    }
+//                }
+//
+//            }
+//
+//        }
+//        System.out.println(dict.indexOf("I"));
+//    }
+
