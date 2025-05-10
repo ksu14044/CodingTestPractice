c5299828a377ae90810f5f47c19194e126de836e
ksu14044 <mhm1404@naver.com>
2025-05-10 16:18:15 +0900

    2 * n 타일링

---
+++ src/main/java/Solutions/nx2타일링.java
@@ -0,0 +1,15 @@
+package Solutions;
+
+public class nx2타일링 {
+    public static void main(String[] args) {
+        int n = 4;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        System.out.println(dp[n]);
+    }
+
+}
