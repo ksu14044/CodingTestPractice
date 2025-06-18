c2dd290a1f9a167cf509d757e57484fa7c719fc9
ksu14044 <mhm1404@naver.com>
2025-06-18 18:07:25 +0900

    거스름돈

---
+++ src/main/java/Solutions/거스름돈.java
@@ -0,0 +1,21 @@
+package Solutions;
+
+class 거스름돈 {
+    public static int solution(int n, int[] money) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        for(int coin : money){
+            for(int i = coin ; i <= n ; i++){
+                dp[i] += dp[i - coin];
+                dp[i] %= 1_000_000_007;
+            }
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int[] money = {1, 2, 5};
+        System.out.println(solution(n, money));
+    }
+}
